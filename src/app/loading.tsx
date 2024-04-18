0dfbbfd78f2aa3d354339fd09efaf8a3997593d9
davigvalerio <al.davigvalerio@gmail.com>
2024-04-18 11:17:41 -0300

    feat(loading): add loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,6 @@
+import { NextPage } from 'next';
+
+const Loading: NextPage = () => <p>Carregando...</p>;
+
+// eslint-disable-next-line no-restricted-syntax
+export default Loading;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import { NextPage } from 'next';
 
-const Home: NextPage = () => <h1>Hello World!</h1>;
+const Home: NextPage = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  const response = await fetch('https://api.github.com/users/dgvalerio');
+
+  const user = await response.json();
+
+  return <pre>{JSON.stringify(user, null, 2)}</pre>;
+};
 
 export default Home;
